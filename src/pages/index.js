3099eaa74f6f0f4af67161c482abdd02aa3843c7
Natalia Burdyey <nata.burdey@gmail.com>
2023-07-11 11:03:01 -0400

    set up router and navbar

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import About from "./AboutPage";
+import AuthWrapper from "./AuthWrapper";
+import Cart from "./CartPage";
+import Checkout from "./CheckoutPage";
+import Error from "./ErrorPage";
+import Home from "./HomePage";
+import PrivateRoute from "./PrivateRoute";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+
+export {
+  About,
+  AuthWrapper,
+  Cart,
+  Checkout,
+  Home,
+  Error,
+  PrivateRoute,
+  Products,
+  SingleProduct,
+};
