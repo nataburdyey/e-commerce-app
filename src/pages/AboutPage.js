6e5c780188b7afa3db3bc12fb5920249447e14c3
Natalia Burdyey <nata.burdey@gmail.com>
2023-07-11 18:24:47 -0400

    add about page and hero component

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,33 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title="about"/>
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>Our Story</h2>
+            <div className="underline"></div>
+            <p>
+              From humble beginnings, our comfy furniture rose to fame,
+              conquering living rooms one cozy seat at a time. Join our comfort
+              revolution and claim your spot in history!
+            </p>
+            <p>
+              Sit on our comfy furniture and experience a sensation so heavenly,
+              you'll think clouds went into the furniture-making business!
+            </p>
+          </div>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +55,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
