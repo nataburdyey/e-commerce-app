79602e95e1e15c167456d3c4d2f39913d351d169
Natalia Burdyey <nata.burdey@gmail.com>
2023-07-11 12:39:23 -0400

    add error page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,9 +1,19 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 const ErrorPage = () => {
-  return <h4>error page</h4>
-}
+  return (
+    <Wrapper className="page-100">
+      <section>
+        <h2>404 Error</h2>
+        <h3>
+          Oops! It seems you've wandered off the digital map.
+        </h3>
+        <Link to="/" className="btn">Back Home</Link>
+      </section>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.main`
   background: var(--clr-primary-10);
@@ -18,6 +28,6 @@ const Wrapper = styled.main`
     text-transform: none;
     margin-bottom: 2rem;
   }
-`
+`;
 
-export default ErrorPage
+export default ErrorPage;
