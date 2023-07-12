b60aaf4b765c892417f7cc2a3569f4b7a9b98381
Natalia Burdyey <nata.burdey@gmail.com>
2023-07-12 12:12:15 -0400

    set stars programmatic approach

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -6,7 +6,7 @@ import { useCartContext } from '../context/cart_context'
 import AmountButtons from './AmountButtons'
 
 const AddToCart = () => {
-  return <h4>addToCart </h4>
+  return <Wrapper>addToCart </Wrapper>
 }
 
 const Wrapper = styled.section`
