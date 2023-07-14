a8b576aed079a25f4b5f441e08c76aa42cd3d3bd
Natalia Burdyey <nata.burdey@gmail.com>
2023-07-14 17:26:17 -0400

    fix warning

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -25,10 +25,11 @@ const FeaturedProducts = () => {
         <h2>Featured Products</h2>
       </div>
       <div className="section-center featured">
-        {featured.slice(0,3).map((product)=> {
-          return <Product key={product.id} {...product} />
+        {featured.slice(0, 3).map((product) => {
+          return <Product key={product.id} {...product} />;
         })}
       </div>
+      <Link to="'products" className="btn">All Products</Link>
     </Wrapper>
   );
 };
